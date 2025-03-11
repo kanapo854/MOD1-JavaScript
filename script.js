b341f4ab8717003b5cf4354f9d69510f1f3cc02c
kanapo854 <quispechaconvalheryleaylean@gmail.com>
2025-03-10 22:31:20 -0400

    Se corrgio errores de html y se agrego las funciones para los botones de volver y ver detalles

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Obtener elementos del DOM
-constverDetallesButton=document.getElementById('verDetalles');
-constvolverButton=document.getElementById('volver');
-consttarjeta=document.querySelector('.tarjeta');
+const verDetallesButton = document.getElementById('verDetalles');
+const volverButton = document.getElementById('volver');
+//const tarjeta = document.querySelector('.tarjeta');
+const cara_posterior = document.querySelector('.posterior');
+const cara_frontal = document.querySelector('.frontal');
+
 // Función para voltear la tarjeta
 verDetallesButton.addEventListener('click', () => {
-tarjeta.style.transform='rotateY(180deg)'; // Voltear la tarjeta
+    //tarjeta.style.transform='rotateY(180deg)'; // Voltear la tarjeta
+    cara_frontal.style.transform = 'rotateY(180deg)';
+    cara_posterior.style.transform = 'rotateY(0deg)';
 });
+
 // Función para volver a la cara frontal
 volverButton.addEventListener('click', () => {
-tarjeta.style.transform='rotateY(0deg)'; // Volver a la cara inicial
+    //tarjeta.style.transform='rotateY(0deg)'; // Volver a la cara inicial
+    cara_frontal.style.transform = 'rotateY(0deg)';
+    cara_posterior.style.transform = 'rotateY(180deg)';
 });
